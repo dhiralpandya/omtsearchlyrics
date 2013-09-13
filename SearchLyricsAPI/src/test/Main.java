8274230a7f46c624ff68623e9a88cd54c271cb00
omt <dhiralpandya@gmail.com>
2013-09-14 02:59:18 +0700

    Test with service

--- SearchLyricsAPI/src/test/Main.java
+++ SearchLyricsAPI/src/test/Main.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import com.omt.lyrics.SearchLyrics;
 import com.omt.lyrics.beans.Lyrics;
+import com.omt.lyrics.beans.LyricsServiceBean;
 import com.omt.lyrics.beans.SearchLyricsBean;
 import com.omt.lyrics.exception.SearchLyricsException;
 import com.omt.lyrics.util.Sites;
@@ -39,43 +40,43 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		SearchLyrics searchLyrics = new SearchLyrics();
-		SearchLyricsBean bean = new SearchLyricsBean();
-		bean.setSongName("lucky one");
-		// bean.setSongAlbum("passenger");
-		bean.setTopMaxResult(1);
-		bean.setSongArtist("Taylor swift");
-		bean.setSites(Sites.SONGMEANINGS);
-
-		List<Lyrics> lyrics;
-		try {
-			lyrics = searchLyrics.searchLyrics(bean);
-			for (Lyrics lyric : lyrics) {
-				System.out.println("Link :" + lyric.getLink());
-				System.out.println("Site :" + lyric.getSites().getName());
-				System.out.println("Text :" + lyric.getText());
-			}
-		} catch (SearchLyricsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		SearchLyricsBean bean = new SearchLyricsBean();
+//		bean.setSongName("lucky one");
+//		// bean.setSongAlbum("passenger");
+//		bean.setTopMaxResult(1);
+//		bean.setSongArtist("Taylor swift");
+//		bean.setSites(Sites.SONGMEANINGS);
+//
+//		List<Lyrics> lyrics;
+//		try {
+//			lyrics = searchLyrics.searchLyrics(bean);
+//			for (Lyrics lyric : lyrics) {
+//				System.out.println("Link :" + lyric.getLink());
+//				System.out.println("Site :" + lyric.getSites().getName());
+//				System.out.println("Text :" + lyric.getText());
+//			}
+//		} catch (SearchLyricsException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
 
 		// Search By Service
 
-		// LyricsServiceBean bean = new LyricsServiceBean();
-		// bean.setSongName("let her go");
-		// bean.setSongAlbum("passenger");
-		//
-		// List<Lyrics> lyrics;
-		// try {
-		// lyrics = searchLyrics.searchLyrics(bean);
-		// for (Lyrics lyric : lyrics) {
-		// System.out.println("Text :" + lyric.getText());
-		// }
-		//
-		// } catch (SearchLyricsException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+		 LyricsServiceBean bean = new LyricsServiceBean();
+		 bean.setSongName("let her go");
+		 bean.setSongAlbum("passenger");
+		
+		 List<Lyrics> lyrics;
+		 try {
+		 lyrics = searchLyrics.searchLyrics(bean);
+		 for (Lyrics lyric : lyrics) {
+		 System.out.println("Text :" + lyric.getText());
+		 }
+		
+		 } catch (SearchLyricsException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
 
 		// test2();
 
